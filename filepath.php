2b1b2f9830780c85427e9a87ae1e11f75232d843
Lavanya612 <71119883+Lavanya612@users.noreply.github.com>
2020-09-11 15:31:32 +0530

    lavanya change
    lavanya change

--- filepath.php
+++ filepath.php
@@ -5,7 +5,7 @@
 /home/mprkvco/public_html/controllers/front/IdentityController.php
 /home/mprkvco/public_html/themes/warehouse/templates/customer/_partials/customer-form.tpl
 /home/mprkvco/public_html/themes/warehouse/templates/customer/_partials/customer-form-identity.tpl
-/home/mprkvco/public_html/themes/warehouse/assets/js/custom-account.js/home/mprkvco/public_html/themes/warehouse/assets/js/custom-account.js
+/home/mprkvco/public_html/themes/warehouse/assets/js/custom-account.js
 
 
 //lavanya test change git 2.56pm
